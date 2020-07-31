c400a0e59ff84597c41b87ecd4c3b02f09ce55d5
Michael Tyson <michaeltyson@outlook.com>
2020-07-31 08:10:39 -0400

    add fetchGraphQL

---
+++ frontend/src/services/fetch-graphql.tsx
@@ -0,0 +1,18 @@
+async function fetchGraphQL(text: string, variables: any) {
+    // Fetch data from GitHub's GraphQL API:
+    const response = await fetch("https://localhost:5001/graphql", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            query: text,
+            variables,
+        }),
+    });
+
+    // Get the response as JSON
+    return await response.json();
+}
+
+export default fetchGraphQL;
