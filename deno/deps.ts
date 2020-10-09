e1fc4290021d3e952066f938963b0090ed7cae0c
Michael Tyson <michaeltyson@outlook.com>
2020-10-09 08:07:11 -0400

    initial deno docker container

---
+++ deno/deps.ts
@@ -0,0 +1,10 @@
+export {
+    Application,
+    Router,
+    RouterContext,
+} from "https://deno.land/x/oak@v6.3.1/mod.ts";
+export {
+    applyGraphQL,
+    gql,
+    GQLError,
+} from "https://deno.land/x/oak_graphql@0.6.2/mod.ts";
