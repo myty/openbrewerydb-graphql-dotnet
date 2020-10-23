c79882d27e8241b089a5430d7dcff09e56a86684
Michael Tyson <michaeltyson@outlook.com>
2020-10-23 00:04:13 -0400

    update dataloader for hc11

--- dotnet/OpenBreweryDB.API/GraphQL/Breweries/Dataloaders/BreweryByIdDataLoader.cs
+++ dotnet/OpenBreweryDB.API/GraphQL/Breweries/Dataloaders/BreweryByIdDataLoader.cs
@@ -1,28 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AndcultureCode.CSharp.Core.Enumerations;
+using AndcultureCode.CSharp.Core.Extensions;
+using GreenDonut;
+using HotChocolate.DataLoader;
+using OpenBreweryDB.API.GraphQL.Errors;
 using OpenBreweryDB.Core.Conductors.Breweries.Interfaces;
 using OpenBreweryDB.Data.Models;
-using AndcultureCode.CSharp.Core.Extensions;
-using System.Linq;
-using OpenBreweryDB.API.GraphQL.Errors;
-using GreenDonut;
-using ACModels = AndcultureCode.CSharp.Core.Models;
-using AndcultureCode.CSharp.Core.Enumerations;
+using ACErrors = AndcultureCode.CSharp.Core.Models.Errors;
 
 namespace OpenBreweryDB.API.GraphQL.Breweries.Dataloaders
 {
-    public class BreweryByIdDataLoader : DataLoaderBase<long, Brewery>
+    public class BreweryByIdDataLoader : BatchDataLoader<long, Brewery>
     {
         private readonly IBreweryConductor _breweryConductor;
 
-        public BreweryByIdDataLoader(IBreweryConductor breweryConductor)
+        public BreweryByIdDataLoader(
+            IBatchScheduler batchScheduler,
+            IBreweryConductor breweryConductor)
+            : base(batchScheduler)
         {
             _breweryConductor = breweryConductor ?? throw new ArgumentNullException(nameof(breweryConductor));
         }
 
-        protected override async Task<IReadOnlyList<Result<Brewery>>> FetchAsync(
+        protected override async Task<IReadOnlyDictionary<long, Brewery>> LoadBatchAsync(
             IReadOnlyList<long> keys,
             CancellationToken cancellationToken)
         {
@@ -32,7 +36,7 @@ namespace OpenBreweryDB.API.GraphQL.Breweries.Dataloaders
             {
                 return breweryResult.ResultObject
                     .Select(Result<Brewery>.Resolve)
-                    .ToList();
+                    .ToDictionary(t => t.Value.Id, t => t.Value);
             }
 
             if (breweryResult.Errors?.Any() == true)
@@ -40,16 +44,16 @@ namespace OpenBreweryDB.API.GraphQL.Breweries.Dataloaders
                 return breweryResult
                     .Errors
                     .Select(e => Result<Brewery>.Reject(new ResultException(e)))
-                    .ToList();
+                    .ToDictionary(t => t.Value.Id, t => t.Value);
             }
 
             return await Task.FromResult(new List<Result<Brewery>> {
-                Result<Brewery>.Reject(new ResultException(new ACModels.Error {
+                Result<Brewery>.Reject(new ResultException(new ACErrors.Error {
                     ErrorType = ErrorType.Error,
                     Key = "NotFound",
                     Message = "Resource Not Found"
                 }))
-            });
+            }.ToDictionary(t => t.Value.Id, t => t.Value));
         }
     }
 }
