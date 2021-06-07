16d6d2f4f6fea2c3ed5a4519872c40801ec04808
Michael Tyson <michaeltyson@outlook.com>
2021-06-07 08:02:41 -0400

    calc page size and startingIndex for connections

--- dotnet/OpenBreweryDB.Schema/OpenBreweryQuery.cs
+++ dotnet/OpenBreweryDB.Schema/OpenBreweryQuery.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using GraphQL;
+using GraphQL.Builders;
+using GraphQL.Relay.Types;
 using GraphQL.Types;
 using OpenBreweryDB.Data.Models;
 using OpenBreweryDB.Schema.Resolvers;
@@ -62,9 +66,80 @@ namespace OpenBreweryDB.Schema
                 .Argument<FloatGraphType>("latitude", "latitude")
                 .Argument<FloatGraphType>("longitude", "longitude")
                 .Argument<IntGraphType, int>("within", "search radius in miles", 25)
-                .Resolve(context => _breweryResolver
-                    .ResolveNearbyBreweries(context)
-                    .ToOffsetConnection(context));
+                .Resolve(context =>
+                {
+                    var query = _breweryResolver.ResolveNearbyBreweries(context).ResultObject;
+                    var totalCount = query.Count();
+                    var (startingIndex, pageSize) = GetStartingIndexAndPageSize(5, totalCount, context);
+
+                    return ConnectionUtils.ToConnection(
+                        query.Take(pageSize),
+                        context,
+                        startingIndex,
+                        totalCount);
+                });
+        }
+
+        private (int startingIndex, int pageSize) GetStartingIndexAndPageSize(int defaultPageSize, int totalCount, IResolveConnectionContext<object> context)
+        {
+            var (first, last) = GetFirstLast(context);
+            var pageSize = last ?? first ?? defaultPageSize;
+
+            if (!string.IsNullOrEmpty(context.After))
+            {
+                var after = ConnectionUtils.CursorToOffset(context.After);
+
+                if (last.HasValue)
+                {
+                    var startingIndex = totalCount - last.Value;
+
+                    return (
+                        startingIndex: startingIndex < after ? after : startingIndex,
+                        pageSize);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(context.Before))
+            {
+                var before = ConnectionUtils.CursorToOffset(context.Before);
+
+                if (first.HasValue)
+                {
+                    var lastIndex = pageSize - 1;
+
+                    return (
+                        startingIndex: 0,
+                        pageSize: lastIndex > before ? before - 1 : pageSize);
+                }
+
+                if (last.HasValue)
+                {
+                    var startingIndex = before - pageSize - 1;
+                    var lastIndex = startingIndex + pageSize - 1;
+
+                    return (
+                        startingIndex: startingIndex < 0 ? 0 : startingIndex,
+                        pageSize: lastIndex > before ? lastIndex - startingIndex : pageSize);
+                }
+            }
+
+            return (
+                startingIndex: 0,
+                pageSize);
+        }
+
+        private (int? first, int? last) GetFirstLast(IResolveConnectionContext<object> context)
+        {
+            if (context.Last == null)
+            {
+                return (
+                    first: context.First,
+                    last: context.First.HasValue ? null : context.Last);
+            }
+
+            return (
+                first: context.First.HasValue ? context.First : null,
+                last: context.First.HasValue ? null : context.Last);
         }
     }
 }
