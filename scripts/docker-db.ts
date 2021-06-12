b83e92b8ec590a1f70f2a51891e47c8ad55f21b8
Michael Tyson <michaeltyson@outlook.com>
2021-06-11 22:48:39 -0400

    update docker script

--- scripts/docker-db.ts
+++ scripts/docker-db.ts
@@ -1,6 +1,7 @@
 import { Docker, Options } from "docker-cli-js";
 import path from "path";
 import { program } from "commander";
+import { grep } from "shelljs";
 
 var currentWorkingDirectory = path.join(__dirname, "..");
 
@@ -26,6 +27,20 @@ const runSqlDockerContainer = async (containerName: string): Promise<void> => {
         await docker.command(
             `run --name ${containerName} -e ACCEPT_EULA=Y -e SA_PASSWORD=passw0rd! -p 9433:1433 -v sqlvolume:/var/opt/mssql -d mcr.microsoft.com/mssql/server:2019-latest`,
         );
+
+        // TODO - DUPLICATE THE FOLLOWING
+        /*
+            #!/bin/bash
+
+            while ! docker logs container | grep -q "[services.d] done.";
+            do
+                sleep 1
+                echo "working..."
+            done
+        */
+
+        // const logs = await docker.command(`logs ${containerName}`);
+        // grep
     }
 };
 
@@ -46,7 +61,7 @@ program
         DEFAULT_CONTAINER_NAME,
     )
     .option("-s, --start", "Starts the database container")
-    .option("-t, --stop", "Starts the database container")
+    .option("-t, --stop", "Stops the database container")
     .action(async (cmdObj) => {
         if (cmdObj.start) {
             console.log("Starting docker container...");
