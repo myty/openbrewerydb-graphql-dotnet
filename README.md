5182253d827fd6a2a9fe641a85c9e8d332f25888
Michael Tyson <myty@users.noreply.github.com>
2021-06-15 22:13:13 -0400

    Improve getting started experience (#13)

--- README.md
+++ README.md
@@ -10,23 +10,21 @@ This project is a learning tool and playground for ideas and concepts around Gra
 
 ## Getting Started
 
-To get started, will need all of the prerequisites installed, then run this from the command line:
+This is using the latest bits from the graphql-dotnet/relay project and since that is not released on nuget yet, it is being referenced via a git submodules. Make sure you run this before you start on the dotnet portion of the code:
+
+    git submodule update --init --recursive
+
+To get started, all of the prerequisites will need to be installed; and then run this from the command line:
 
     yarn setup
 
 ### Server (.NET Core)
 
-This is using the latest bits from the graphql-dotnet/relay project and since that is not released on nuget yet, it is being referenced via a git submodules. Make sure you run this before you start on the dotnet portion of the code:
-
-    git submodule update --init --recursive
-
-To start the GraphQL server, run this (The first time this runs, it will download seed data so it may take a few minutes more to get things spun up):
-
-    yarn db:start
-
-For the time being, wait 10-15 seconds to allow the SQL instance to boot up and get into a ready state. Once SQL is ready, you can then run
+To start the GraphQL server, run:
 
     yarn dotnet
+
+The first time this runs, it will download a SQL docker container, start it, and finally download seed data. Keep all of these inital steps in mind, since it will most liklet take a few minutes to get things spun up) _(Also, if you are running on Windows, make sure Docker is set for Linux containers)_
 
 Once you see `Application started. Press Ctrl+C to shut down.`, go to this url, https://localhost:5001/api/graphql and play around with the GraphQL data and schema. It is using the Altair UI.
 
--- scripts/docker-db.ts
+++ scripts/docker-db.ts
@@ -1,35 +1,117 @@
 import { Docker, Options } from "docker-cli-js";
 import path from "path";
 import { program } from "commander";
+import { exec, execSync, spawn } from "child_process";
 
 var currentWorkingDirectory = path.join(__dirname, "..");
 
 const dockerOptions = new Options(
     /* machineName */ undefined,
     /* currentWorkingDirectory */ currentWorkingDirectory,
-    /* echo*/ false,
+    /* echo*/ true,
 );
 
 const docker = new Docker(dockerOptions);
 
-const containerName = "sql-test-db";
+const DEFAULT_CONTAINER_NAME = "openbrewerydb-sql-db";
 
-const findDockerContainer = async (name: string) => {
-    const data = await docker.command("ps");
-    const containerList: any[] = data.containerList ?? [];
-    return containerList.find((c) => c.names === name);
+type ContainerState = "exited" | "running";
+
+interface ContainerListItem {
+    Command: string;
+    CreatedAt: string;
+    ID: string;
+    Image: string;
+    Labels: string;
+    LocalVolumes: string;
+    Mounts: string;
+    Names: string;
+    Networks: string;
+    Ports: string;
+    RunningFor: string;
+    Size: string;
+    State: ContainerState;
+    Status: string;
+}
+
+const findDockerContainer = async (
+    name: string,
+): Promise<ContainerListItem | undefined> => {
+    const containerList = await getContainerList();
+    return containerList.find((c) => c.Names === name);
 };
 
-const runSqlDockerContainer = async (): Promise<void> => {
+const getContainerList = async (): Promise<Array<ContainerListItem>> =>
+    new Promise((resolve) => {
+        const parsedContainerList: Array<ContainerListItem> = [];
+
+        spawn("docker", ["container", "ls", "-a", "--no-trunc", "--format='{{json .}}'"])
+            .stdout?.on("data", (chunk: Buffer) => {
+                const chunkedString = chunk.toString();
+                const jsonString = chunkedString.substring(1, chunkedString.length - 2);
+
+                parsedContainerList.push(JSON.parse(jsonString));
+            })
+            .on("close", () => {
+                resolve(parsedContainerList);
+            });
+    });
+
+const runSqlDockerContainer = async (containerName: string): Promise<void> => {
     const dockerContainer = await findDockerContainer(containerName);
+
+    if (dockerContainer?.State === "running") {
+        return;
+    }
+
     if (dockerContainer == null) {
         await docker.command(
             `run --name ${containerName} -e ACCEPT_EULA=Y -e SA_PASSWORD=passw0rd! -p 9433:1433 -v sqlvolume:/var/opt/mssql -d mcr.microsoft.com/mssql/server:2019-latest`,
         );
     }
+
+    if (dockerContainer?.State === "exited") {
+        await docker.command(`start ${containerName}`);
+    }
+
+    await waitForLogs(containerName, /Server is listening on(.*)/);
 };
 
-const shutdownSqlDockerContainer = async (): Promise<void> => {
+const waitForLogs = async (containerName: string, logPattern: RegExp): Promise<void> => {
+    try {
+        const containerLogs = spawn(
+            "docker",
+            ["logs", containerName, "-f", "--since=10s"],
+            {
+                shell: false,
+            },
+        );
+
+        await new Promise<void>((resolve) => {
+            containerLogs.stdout.on("data", (chunk: string) => {
+                const matches = logPattern.exec(chunk) ?? [];
+                if (matches.length > 0) {
+                    resolve();
+                }
+            });
+        });
+
+        killPID(containerLogs.pid);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const killPID = (pid: number) => {
+    var isWin = /^win/.test(process.platform);
+    if (!isWin) {
+        process.kill(pid);
+    } else {
+        exec("taskkill /PID " + pid + " /T /F");
+    }
+};
+
+const shutdownSqlDockerContainer = async (containerName: string): Promise<void> => {
     const foundContainer = await findDockerContainer(containerName);
 
     if (foundContainer != null) {
@@ -40,18 +122,23 @@ const shutdownSqlDockerContainer = async (): Promise<void> => {
 
 program
     .command("db")
+    .option(
+        "-n, --container-name",
+        `Container name (defaults to ${DEFAULT_CONTAINER_NAME})`,
+        DEFAULT_CONTAINER_NAME,
+    )
     .option("-s, --start", "Starts the database container")
-    .option("-t, --stop", "Starts the database container")
+    .option("-t, --stop", "Stops the database container")
     .action(async (cmdObj) => {
         if (cmdObj.start) {
             console.log("Starting docker container...");
-            await runSqlDockerContainer();
+            await runSqlDockerContainer(cmdObj.containerName);
             return;
         }
 
         if (cmdObj.stop) {
             console.log("Shutting down docker container...");
-            await shutdownSqlDockerContainer();
+            await shutdownSqlDockerContainer(cmdObj.containerName);
         }
     });
 
